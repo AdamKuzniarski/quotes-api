20b9a8d01d437dca9a22e4fafa4aff851c061780
AdamKuzniarski <adam.kuzniarski@gmx.de>
2025-10-27 08:43:54 +0100

    small changes

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { UserService } from './user/user.service';
 
 @Injectable()
 export class AppService {
+  constructor(private readonly userService: UserService) {}
+
   getHello(): string {
-    return 'Hello Julia!';
+    return 'Hello Julia!' + this.userService.getUserName();
   }
 }
